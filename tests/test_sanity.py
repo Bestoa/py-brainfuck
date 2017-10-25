9f044b8b89fbcf9564f1b397d30d8f9932fd185c
Besto Apache <bestoapache@gmail.com>
2017-10-25 13:26:47 +0800

    Add unit test

---
+++ tests/test_sanity.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+import sys
+import nbfi
+from tempfile import TemporaryFile
+
+def test_print_prime():
+    result = ''
+    with open('demo/print_prime', 'r') as fin:
+        with TemporaryFile('w+t') as fout:
+            stdin = sys.stdin
+            stdout = sys.stdout
+            sys.stdin = fin
+            sys.stdout = fout
+            nbfi.run()
+            sys.stdout = stdout
+            sys.stdin = stdin
+            fout.seek(0)
+            result = fout.read()
+
+    with open('tests/except_result', 'r') as f:
+        except_result = f.read()
+        assert except_result == result
